55a8bd7b655744eefe268fa1d16cba8a5c2f14a5
Diego Trigueros <diegofytn@gmail.com>
2016-02-22 14:00:08 -0600

    Protocolo2016
    Proyecto para comisión de protocolo

---
+++ php/main.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Página principal</title>
+	<link rel="stylesheet" href="../css/style.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+	<div class="contenedor">
+		<nav class="menu">
+			<div class="core">
+				<div class="calendar">
+					<i class="material-icons md-36">event</i>
+				</div>
+				<div class="add">
+					<i class="material-icons">alarm_add</i
+				</div>
+				<div class="assign">
+					<i class="material-icons">assignment</i>
+				</div>
+				<div class="manage">
+					<i class="material-icons">build</i>
+				</div>
+				<div class="profile">
+					<i class="material-icons">account_circle</i>
+				</div>
+			</div>	
+		</nav>
+	</div>
+</body>
+</html>
